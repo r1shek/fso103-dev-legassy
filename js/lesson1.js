58b8f8f242876b0d019703f7f609cbb087e2af57
r1shek <rostic523@gmail.com>
2024-02-24 18:23:55 +0100

    1

---
+++ js/lesson1.js
@@ -0,0 +1,97 @@
+// console.log('Hello');
+// alert('world');
+
+// let num;
+// console.log('Hello');
+
+// Tak ne robimo!!!
+// const $ = 11;
+// console.log($);
+
+// const $num = 11;
+// console.log($num);
+
+// const num$ = 11;
+// console.log(num$);
+
+// const _ = 11;
+// console.log(_);
+
+// const num1 = 11;
+// console.log(num1);
+
+// const first_name = "Mango";
+
+//Tak ne pracue
+
+// const 1num = "hi";
+
+// const first-name = "Mango";
+
+// const let = "hi";
+
+// const const = "hi";
+
+// const false = "hi";
+
+//TAK TREBA!!!
+
+// const firstName = "Mango";
+
+//Tipi dannih
+
+// const number = 10;
+// const string = 'Mango';
+// const bool = true;
+// const someDate = null;
+// const anotherDate = undefined;
+
+// console.log(typeof number);
+// console.log(typeof string);
+// console.log(typeof bool);
+// console.log(typeof someDate);
+// console.log(typeof anotherDate);
+
+//Chisla
+
+// console.log(2 + 2);
+// console.log(2 - 3);
+// console.log(2 * 3);
+// console.log(2 / 2);
+
+// console.log(2 / 0);
+// console.log(5 / undefined);
+// console.log(2 / false);
+// console.log(5 / '10');
+// console.log(5 / 'hello');
+
+//Konkatinaciya
+
+// console.log(5 + '5');
+// console.log(10 + '20' - 1000);
+// console.log(10 - 1000 - '20');
+// console.log(10 - 1000 + '20');
+
+// isNaN perevirka
+
+// const value = 5 / '20px';
+// console.log(isNaN(value));
+// console.log(!isNaN(value));
+// console.log(isNaN(5 / '20px'));
+
+//Drobni chisla
+
+// console.log(0.1 + 0.2);
+// console.log(Number((0.1 + 0.2).toFixed(1)));
+// console.log(typeof Number((0.1 + 0.2).toFixed(1)));
+
+// Number
+
+// const num = '20,45px';
+// const normalizedString = num.replace(',', '.');
+// console.log(num);
+// console.log(+num);
+
+// console.log(Number(num));
+// console.log(Number.parseFloat(normalizedString));
+// console.log(Number.parseInt(normalizedString));
