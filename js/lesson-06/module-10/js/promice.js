9adbeed145047bea2b1ace83fdd4f7cabfb94375
smelik81 <artem.smelyanskiy81@gmail.com>
2024-05-04 19:56:46 +0300

    add task Promise

--- js/lesson-06/module-10/js/promice.js
+++ js/lesson-06/module-10/js/promice.js
@@ -4,9 +4,16 @@
  * через 2 секунди після виклику функції
  */
 
-// function greet() {
-//   return 'hello world';
-// }
+ /* function greet() {
+     return new Promise((resolve, reject) => {
+         setTimeout(() => {
+           resolve("hello world");
+       }, 2000)
+   })
+};
+greet().then(result => console.log(result))
+ */
+
 
 //TODO:====================02==========================
 /**
@@ -17,4 +24,68 @@
  * Якщо значення не парне, вирішуй проміс і повертай "odd" через 2 секунди.
  */
 
-// const value = prompt('Paste value');
+/* function promiseNumber() {
+    return new Promise((resolve, reject) => {
+        const value = prompt('Paste value');
+        if (isNaN(value)) {
+            return reject('Hallo');
+        } else if (value % 2 === 0) {
+            setTimeout(() => {
+               return resolve('even')
+            }, 1000) 
+        } else {
+            setTimeout(() => {
+               return resolve('odd')
+            }, 2000)
+        }
+    })
+};
+
+promiseNumber()
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
+ */
+
+
+
+
+const list = document.querySelector(".js-ul");
+
+
+function getPhotos() {
+    const BASE_URL = "https://jsonplaceholder.typicode.com";
+    const END_POINT = '/photos';
+    const params = new URLSearchParams({
+        _limit: 20
+    })
+    return fetch(`${BASE_URL}${END_POINT}?${params}`)
+        .then(
+     (response) => {
+       if (!response.ok) {
+         throw new Error(response.status);
+       }
+       return response.json();
+     }
+   );
+}
+
+
+function createMarkup(arr) {
+    return arr.reduce((html, { url, title }) => {
+        return (html += `
+        <li>
+          <img src="${url}" alt="${title}">
+        </li>
+        `);
+    }, "")
+};
+console.log('початок завантаження');
+    
+        getPhotos()
+            .then(response => {
+               list.insertAdjacentHTML('beforeend', createMarkup(response))
+            })
+        .catch((error) => console.log(error))
+            .finally(() => console.log('Кінець завантаження'))
+
+
